87970e37bc2654547538a4a8a7a35aaf4d161d60
Annisa Kurniawati <annisak0708@gmail.com>
2025-05-24 16:05:03 +0700

    kelola-lomba

--- resources/views/admin/daftar-lomba.blade.php
+++ resources/views/admin/daftar-lomba.blade.php
@@ -1,5 +1,99 @@
 @extends('layout.template')
 
 @section('content')
+    <main class="flex-1 p-10">
+        <div class="w-full mx-auto p-6 border border-gray-200 rounded-lg">
+            <h2 class="text-xl font-semibold mb-4">Katalog Lomba</h2>
+            <div class="flex flex-wrap gap-4 py-4 items-center">
+                <!-- Search Input -->
+                <div class="relative">
+                    <input type="text" placeholder="Cari disini"
+                        class="pl-10 pr-4 py-2 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm" />
 
+                </div>
+
+                <!-- Dropdown: Kategori -->
+                <div class="relative w-54">
+                    <select
+                        class="appearance-none w-full py-2 pr-4 pl-4 border border-gray-200 rounded-md text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        <option disabled selected hidden>Pilih Kategori</option>
+                        <option>Cyber Security</option>
+                        <option>IoT</option>
+                        <option>Software Development</option>
+                    </select>
+                </div>
+
+                <!-- Dropdown: Tingkat Lomba -->
+                <div class="relative w-40">
+                    <select
+                        class="appearance-none w-full py-2 pr-10 pl-4 border border-gray-200 rounded-md text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        <option disabled selected hidden>Pilih Tingkat</option>
+                        <option>Internasional</option>
+                        <option>Nasional</option>
+                        <option>Provinsi</option>
+                        <option>Kota/Kabupaten</option>
+                        <option>Internal</option>
+                    </select>
+                </div>
+                <!-- Dropdown: Partisipan -->
+                <div class="relative w-44">
+                    <select
+                        class="appearance-none w-full py-2 pr-10 pl-4 border border-gray-200 rounded-md text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        <option disabled selected hidden>Pilih Tipe Peserta</option>
+                        <option>Tim</option>
+                        <option>Individu</option>
+                    </select>
+                </div>
+                <!-- Button: Tambah Lomba -->
+                <div class="ml-auto">
+                    <a href="{{ route('lomba.create') }}">
+                        <button
+                            class="text-sm bg-blue-500 text-white px-5 py-2 rounded-md hover:bg-blue-600 transition flex items-center gap-2">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+                            </svg>
+                            Tambah Lomba
+                        </button>
+                    </a>
+                </div>
+            </div>
+
+
+
+            <div class="mt-4 grid grid-cols-[repeat(auto-fit,_minmax(240px,_1fr))] gap-4">
+                @for($i = 0; $i < 10; $i++)
+                    {{-- <a href="{{ route('lomba.detail' )}}"> --}}
+                        <div
+                            class="h-66 border border-gray-200 rounded-lg transform hover:-translate-y-1 transition duration-300 hover:shadow-lg hover:border-blue-700 ">
+                            <img src="{{ asset('images/poster.jpeg') }}" alt="Banner Event"
+                                class="w-full rounded-t-lg h-30 object-cover object-top">
+                            <div class="p-4">
+                                <p class="text-xs text-blue-600 font-bold">Cyber Security, IoT</p>
+                                <h2 class="text-base font-semibold text-gray-800 mt-1">Hackathon Merdeka Jawa</h2>
+                                <div class="flex items-center text-xs text-gray-600 mt-3 gap-1">
+                                    <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" stroke-width="2"
+                                        viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                    </svg>
+                                    <span>21 Agustus – 22 Agustus 2020</span>
+                                </div>
+
+                                <div class="flex items-center text-xs text-gray-600 mt-1 gap-1">
+                                    <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" stroke-width="2"
+                                        viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M17.657 16.657L13.414 12.414a4 4 0 10-1.414 1.414l4.243 4.243a1 1 0 001.414-1.414z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15 11a4 4 0 11-8 0 4 4 0 018 0z" />
+                                    </svg>
+                                    <span>Institut Teknologi Guatemala</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                @endfor
+            </div>
+        </div>
+    </main>
 @endsection
--- resources/views/admin/tambah-lomba.blade.php
+++ resources/views/admin/tambah-lomba.blade.php
@@ -1,5 +1,111 @@
 @extends('layout.template')
 
 @section('content')
+<div class="max-w-7xl mx-auto mt-6 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto bg-white rounded-lg shadow border border-gray-200 p-6 space-y-6">
+        <h2 class="text-xl font-semibold mb-2">Tambah Lomba Baru</h2>
+        <p class="text-sm text-gray-600 mb-6">
+            Lengkapi data prestasi yang telah kamu raih selama masa studi. Pastikan kamu mengunggah bukti yang valid seperti sertifikat atau surat keterangan resmi.
+        </p>
 
+        <form>
+            <div style="background-color: #1E6AAE; color: white;" class="px-4 py-2 rounded-t-md font-semibold">Data Kompetisi</div>
+                <div class="space-y-6 mt-4">
+                <div>
+                    <label class="block text-sm font-medium text-gray-700">Nama Lomba</label>
+                    <input type="text" name="nama_lomba" placeholder="Input your nama lomba"
+                           class="mt-1 block w-full font-normal text-gray-600 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100">
+                </div>
+
+                <div>
+                    <label class="block text-sm font-medium text-gray-700">Tingkat Lomba</label>
+                    <div class="relative">
+                    <select id="partisipasi" name="partisipasi"
+                           class="mt-1 block w-full font-normal text-gray-400 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100">
+                           <option disabled selected hidden>Pilih Tingkat</option>
+                                <option>Internasional</option>
+                                <option>Nasional</option>
+                                <option>Provinsi</option>
+                                <option>Kota/Kabupaten</option>
+                                <option>Internal</option>
+                            </select>
+                    </div>
+                </div>
+
+                <div>
+                    <label class="block text-sm font-medium text-gray-700">Penyelenggara</label>
+                    <input type="text" name="penyelenggara" placeholder="Input your kategori"
+                           class="mt-1 block w-full font-normal text-gray-600 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100">
+                </div>
+
+                <div>
+                    <label class="block text-sm font-medium text-gray-700">Bidang Lomba (Tag)</label>
+                    <input type="text" name="bidang_lomba" placeholder="Input your kategori"
+                           class="mt-1 block w-full font-normal text-gray-600 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100">
+                </div>
+
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700">Tanggal Mulai Lomba</label>
+                        <input type="date" name="tanggal_mulai"
+                               class="mt-1 block w-full font-normal text-gray-400 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100">
+                    </div>
+
+                    <div>
+                        <label class="block text-sm font-medium text-gray-700">Tanggal Berakhir Lomba</label>
+                        <input type="date" name="tanggal_berakhir"
+                               class="mt-1 block w-full font-normal text-gray-400 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100">
+                    </div>
+                </div>
+
+                <div>
+                    <label class="block text-sm font-medium text-gray-700">Deadline Pendaftaran</label>
+                    <input type="date" name="deadline"
+                           class="mt-1 block w-full font-normal text-gray-400 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100">
+                </div>
+
+                <div>
+                    <label class="block text-sm font-medium text-gray-700">Jumlah Peserta</label>
+                    <input type="url" name="jumlah_peserta" placeholder="Input jumlah peserta"
+                           class="mt-1 block w-full font-normal text-gray-600 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100">
+                </div>
+
+                <div>
+                    <label class="block text-sm font-medium text-gray-700">URL Kompetisi</label>
+                    <input type="url" name="url_kompetisi" placeholder="Input your kategori"
+                           class="mt-1 block w-full font-normal text-gray-600 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100">
+                </div>
+
+                <div>
+                    <label class="block text-sm font-medium text-gray-700">Deskripsi</label>
+                    <textarea name="deskripsi" rows="4" placeholder="Input deskripsi lomba..."
+                              class="mt-1 block w-full font-normal text-gray-600 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100"></textarea>
+                </div>
+
+                <div>
+    <label class="block text-sm font-medium text-gray-700 mb-1">Foto Poster</label>
+
+    <label for="poster" 
+        class="mt-1 block w-full font-normal text-gray-600 border  border-gray-300 rounded-md shadow-sm p-2  focus:ring focus:ring-blue-100">
+        <div class="text-center text-sm">
+            Drag your certificate or 
+            <span class="inline-block px-3 py-1 ml-2 bg-white border border-gray-300 rounded-md shadow-sm text-gray-700 text-sm hover:bg-gray-100">
+                Browse
+            </span>
+        </div>
+        <input id="poster" name="poster" type="file" accept=".jpg,.jpeg,.png,.pdf" class="hidden">
+    </label>
+</div>
+
+
+                <div class="text-right">
+                    <button type="submit"
+                            class="w-32 bg-blue-500 text-white py-2 px-4 rounded-sm hover:bg-blue-700 transition duration-200">
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
