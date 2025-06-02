6260171c468cc181fa9547a57d2e36ba66c0ac8f
FlaBBB <f12345ff67@gmail.com>
2025-06-02 12:09:38 +0700

    fix: unable logout from header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -33,7 +33,7 @@
                     class="hidden absolute right-0 mt-2 w-40 bg-white border rounded shadow-lg z-50 py-2">
                     <a href="{{ route('mahasiswa.profile') }}" class="block px-4 py-2 text-gray-700 hover:bg-blue-50">Profil
                         saya</a>
-                    <form method="POST" action="">
+                    <form method="POST" action="{{ route('logout') }}">
                         @csrf
                         <button type="submit"
                             class="w-full text-left px-4 py-2 text-gray-700 hover:bg-blue-50">Logout</button>
